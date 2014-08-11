d094310b881375407355892a773a96e71a2b4687
kc9zda <kc9zda@gmail.com>
2014-08-10 22:37:18 -0400

    Added Recipe for Exploding Snowball

--- src/main/java/kc9zda/mcmod/rcm/extras/RCMExtras.java
+++ src/main/java/kc9zda/mcmod/rcm/extras/RCMExtras.java
@@ -3,7 +3,10 @@ package kc9zda.mcmod.rcm.extras;
 import kc9zda.mcmod.rcm.RandomCrapMod;
 import kc9zda.mcmod.rcm.extras.entity.EntityExplodingSnowball;
 import kc9zda.mcmod.rcm.extras.item.ItemExplodingSnowball;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -15,7 +18,7 @@ public class RCMExtras {
 	public static void init(FMLInitializationEvent e) {
 		initItems();
 		initEntities();
-		//initRecipes();
+		initRecipes();
 	}
 	
 	public static void initItems() {
@@ -25,5 +28,9 @@ public class RCMExtras {
 
 	public static void initEntities() {
 		EntityRegistry.registerModEntity(EntityExplodingSnowball.class, "Exploding Snowball", 0, RandomCrapMod.instance, 64, 10, true);
+	}
+	
+	public static void initRecipes() {
+		GameRegistry.addRecipe(new ItemStack(snowTnt), "STS", "TST", "STS", 'S', Items.snowball, Blocks.tnt);
 	}
 }
